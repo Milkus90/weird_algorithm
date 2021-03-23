28e08e8721643a658e5e0fe55bbedb9d5e3fe67f
Jacek Milewski <jacek_milewski@hotmail.com>
2021-03-23 23:09:59 +0100

    excercise done

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+
+int main()
+{
+    int n;
+    std::cout << "Podaj liczbe do przeliczenia algorytmem: ";
+    std::cin >> n;
+
+    while (n != 1){
+        std::cout << n << " -> ";
+        if (n%2==0)
+            n = n/2;
+        else
+            n = n*3+1;
+
+    }
+    std:: cout << n << std::endl;
+
+
+    return 0;
+}
